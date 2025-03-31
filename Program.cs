1e4f665065791d1bd68b256cccd53daff79c32c5
Mitchell Boyan <mitchellboyan@gmail.com>
2025-04-01 01:17:58 +1100

    Initial random number generator script

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Number Generator");
+            
+            while (true)
+            {
+                Console.Write("Enter the lower bound of your range: ");
+                int lowerBound = Convert.ToInt32(Console.ReadLine());
+                
+                Console.Write("Enter the upper bound of your range: ");
+                int upperBound = Convert.ToInt32(Console.ReadLine());
+
+                Random random = new Random();
+                int randomNumber = random.Next(lowerBound, upperBound + 1);
+                Console.WriteLine($"Random Number: {randomNumber}");
+
+                Console.Write("\nDo you want to generate another number? (yes/no): ");
+                string userResponse = Console.ReadLine().Trim().ToLower();
+
+                if (userResponse != "yes")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Exiting the program. Goodbye!");
+        }
+    }
+}
